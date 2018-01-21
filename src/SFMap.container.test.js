223c3a2cd09670cb51cf176202d2a2d478e43529
Rahul gaba <rahulgaba16@gmail.com>
2018-01-22 01:57:28 +0700

    Pages added

--- src/SFMap.container.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SFMap from './SFMap.container';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SFMap />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SFMap from './SFMap.container';
-
-ReactDOM.render(<SFMap />, document.getElementById('root'));
