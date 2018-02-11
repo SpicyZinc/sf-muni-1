47fe7c6189ed497b12b4110086b263d1797e1d32
Rahul gaba <rahulgaba16@gmail.com>
2018-02-11 13:42:49 +0530

    Updates

--- worker.js
+++ worker.js
@@ -1,1 +1,17 @@
-console.log('self')
+const origin = 'https://rahulgaba.com/sf-muni'
+const cacheFiles = [`${origin}/fixtures/arteries.json`,`${origin}/fixtures/freeways.json`, `${origin}/fixtures/neighborhoods.json`, `${origin}/fixtures/streets.json`];
+
+self.addEventListener('install', (event) => {
+  caches.delete('sf-muni');
+  event.waitUntil(
+    caches.open('sf-muni')
+      .then((cache)=> {cache.addAll(cacheFiles); console.log('all added')})
+      .then(() => self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', (event) => {
+  if(cacheFiles.includes(event.request.url))
+    return event.respondWith(caches.match(event.request));
+  else return
+})
